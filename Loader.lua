ca5bdcc93c743891a163d516ebd0241d2edfc62e
etfbscript-server <ashiiwng08@gmail.com>
2026-03-01 16:11:56 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,8 +1,12 @@
--- Universal Invincible Flight R15
-loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Invinicible-Flight-R15-45414"))()
-
--- Brainrot Loader 1
-loadstring(game:HttpGet("https://raw.githubusercontent.com/etfbscript-server/EscapeTsunamiForBrainrot/refs/heads/main/Loader.lua"))()
-
--- Brainrot Loader 2
+local function s1()
+end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gumanba/Scripts/main/EscapeTsunamiForBrainrots"))()
+local function s2()
+end
 loadstring(game:HttpGet("https://raw.githubusercontent.com/etfbscript-server/EscapeTsunamiForBrainrots/refs/heads/main/Loader.lua"))()
+local function s3()
+end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/etfbscript-server/EscapeTsunamiForBrainrot/refs/heads/main/Loader.lua"))()
+task.spawn(s1)
+task.spawn(s2)
+task.spawn(s3)
