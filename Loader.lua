be48d575359287ec66139321efccaf1c91a3a436
etfbscript-server <ashiiwng08@gmail.com>
2026-03-01 12:27:20 +0800

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,8 @@
+-- Universal Invincible Flight R15
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Invinicible-Flight-R15-45414"))()
+
+-- Brainrot Loader 1
+loadstring(game:HttpGet("https://raw.githubusercontent.com/etfbscript-server/EscapeTsunamiForBrainrot/refs/heads/main/Loader.lua"))()
+
+-- Brainrot Loader 2
+loadstring(game:HttpGet("https://raw.githubusercontent.com/etfbscript-server/EscapeTsunamiForBrainrots/refs/heads/main/Loader.lua"))()
